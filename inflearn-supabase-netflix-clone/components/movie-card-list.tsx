2f2a4d0fccb595e4adb32161d4bcfa3e15565020
aejin24 <aejin24@naver.com>
2025-01-13 22:08:28 +0900

    [5-3] 무한 스크롤 기능 구현하기, 더 나은 검색을 위한 SEO 작업하기

--- inflearn-supabase-netflix-clone/components/movie-card-list.tsx
+++ inflearn-supabase-netflix-clone/components/movie-card-list.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 import { Spinner } from "@material-tailwind/react";
-import { useQuery } from "@tanstack/react-query";
+import { LastPage } from "@mui/icons-material";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import { searchMovies } from "actions/movieActions";
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
 import { useRecoilValue } from "recoil";
 import { searchState } from "utils/recoil/atoms";
 import MovieCard from "./movie-card";
@@ -10,18 +13,41 @@ import MovieCard from "./movie-card";
 export default function MovieCardList() {
   const search = useRecoilValue(searchState);
 
-  const getAllMoviesQuery = useQuery({
-    queryKey: ["movie", search],
-    queryFn: () => searchMovies(search),
+  const { data, fetchNextPage, hasNextPage, isFetching, isFetchingNextPage } =
+    useInfiniteQuery({
+      initialPageParam: 1,
+      queryKey: ["movie", search],
+      queryFn: ({ pageParam }) =>
+        searchMovies({ search, page: pageParam, pageSize: 12 }),
+      getNextPageParam: (lastPage) =>
+        lastPage.page ? lastPage.page + 1 : null,
+    });
+
+  const { ref, inView } = useInView({
+    threshold: 0,
   });
+
+  useEffect(() => {
+    if (inView && hasNextPage && !isFetching && !isFetchingNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage]);
 
   return (
     <div className="grid gap-1 md:grid-cols-4 grid-cols-3 w-full h-full">
-      {getAllMoviesQuery.isLoading && <Spinner />}
-      {getAllMoviesQuery.data &&
-        getAllMoviesQuery.data.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))}
+      {(isFetching || isFetchingNextPage) && <Spinner />}
+      {
+        <>
+          {data?.pages
+            .map((page) => page.data)
+            ?.flat()
+            ?.map((movie) => (
+              <MovieCard key={movie.id} movie={movie} />
+            ))}
+
+          <div ref={ref} />
+        </>
+      }
     </div>
   );
 }
